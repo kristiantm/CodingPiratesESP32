bcee3c95d75cd07eb294e2fce20209741ea8703f
Kristian <61930466+kristiantm@users.noreply.github.com>
2024-11-20 17:37:07 +0100

    Bugfix
    startScreen.h included as a file instead of library

--- Pong_ESP32_Sprites/Pong_ESP32_Sprites.ino
+++ Pong_ESP32_Sprites/Pong_ESP32_Sprites.ino
@@ -1,7 +1,7 @@
 #include <Wire.h>
 #include <U8g2lib.h>
 
-#include <startScreen.h>
+#include "startScreen.h"
 
 // Initialize U8g2 for SH1106 display with I2C communication
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
